4bee82bf3572732ca6dfccb14ffa76a82da53bf1
Stephman1 <keelers@seas.upenn.edu>
2023-10-13 16:26:19 +0100

    Api key is retrieved from a txt file in parent dir

--- data/get_company_profile.py
+++ data/get_company_profile.py
@@ -1,20 +1,35 @@
 """
 GET request based on company number, 07496944 (THE CO-MISSION CHURCHES TRUST).
+
+The authentication method uses an api key stored in a text file located in the parent directory.
 """
 
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import os
+from urllib.parse import urljoin
 
-username = "ea96b774-700f-4ef6-b69a-592545426b56"
+def getCompanyInfo(company_num: str) -> str:
 
-company_data = requests.get(
-    url='https://api.company-information.service.gov.uk/company/07496944',
-    auth=HTTPBasicAuth(username, '')
+    # Get api key
+    auth_fp = os.path.abspath(os.path.join(os.pardir,os.getcwd()))
+    auth_file = os.path.join(auth_fp, 'authentication.txt')
+    f = open(auth_file,'r')
+    auth_dict = json.loads(f.read())
+    username = auth_dict['api_key']
+
+    company_data = requests.get(
+        url=urljoin('https://api.company-information.service.gov.uk/company/',company_num),
+        auth=HTTPBasicAuth(username, '')
     )
 
-json_object = json.loads(company_data.text)
+    json_object = json.loads(company_data.text)
 
-json_formatted_str = json.dumps(json_object, indent=2)
+    json_formatted_str = json.dumps(json_object, indent=2)
 
-print(json_formatted_str)
+    return json_formatted_str
+    
+if __name__ == '__main__':
+    company = '07496944'    
+    print(getCompanyInfo(company))
