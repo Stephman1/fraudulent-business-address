039fab15524aa4da716d77ebf33c2ba66c005265
Snow Jiang <gingersnow1990@gmail.com>
2024-06-12 21:52:42 +0800

    udpate django views to restframework api views

--- backend/address/views.py
+++ backend/address/views.py
@@ -1,38 +1,37 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
-from django.views.decorators.http import require_GET
-from . import models, serializers
+from rest_framework import viewsets, status
+
 from companies_house.retrieval.companies_house_api import ChAPI
-from django.shortcuts import render
-from rest_framework import viewsets
+from . import models, serializers
 from . import models
 from . import serializers
+
 
 # Create your views here.
 class AddressViewSet(viewsets.ModelViewSet):
   queryset = models.Address.objects.all()
   serializer_class = serializers.AddressSerializer
 
-@require_GET
+
+@api_view(['GET'])
 def get_company_data(request):
     query = request.GET.get('query') 
     size = request.GET.get('size', 25) 
-    print(f"Received query: {query}")  # Debugging
-    print(f"Received size: {size}") 
     url = 'https://api.company-information.service.gov.uk/advanced-search/companies'
     api_key = ChAPI.getApiKey()
+    print(api_key)
     params = {
         "location": query,
         "size": size
     }
 
     if not query:
-        return JsonResponse({'error': 'Address is not provided'}, status=400)
+        return Response({'error': 'Address is not provided'}, status=status.HTTP_400_BAD_REQUEST)
        
     try:
         data = ChAPI.getChData(url=url, api_key=api_key, params=params)
-        response = JsonResponse(data)
-        response['Content-Type'] = 'application/json'
-        return response
+        return Response(data, content_type='application/json')
     except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
